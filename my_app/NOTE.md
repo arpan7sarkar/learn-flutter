3f8dc9c4543bd0d8a7d26a0232c147e3357f5c01
arpan7sarkar <arpansakar@gmail.com>
2025-09-14 21:35:36 +0530

    learned about Material APP

--- my_app/NOTE.md
+++ my_app/NOTE.md
@@ -1,4 +1,4 @@
-# Flutter journey starts here 
+# Flutter journey starts here
 
 flutter create app_name
 flutter run
@@ -9,5 +9,20 @@ https://m3.material.io/ is for learning about material design
 
 for hot relod press r/R
 
-flutter docs are best 
+flutter docs are best
 style api.flutter.dev
+
+Matireal app in default uses ugly colour theme for telling us to use good ui
+
+theme:
+Contains properties for the
+look and feel of our app using
+Material Design, including
+colors and swatches.
+
+Scaffold:
+Here provide Widgets for
+two properties, appBar (a
+navigation bar for the app)
+and body (what inside the
+application).
--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -3,16 +3,24 @@ import 'package:flutter/material.dart';
 const learner='Arpan';
 
 void main() {
-  //runApp does WIDGET --> SCREEN
-  
-  runApp(
-    //Text Widget
-    const Center(
-      child: Text(
-        'Hello world I am $learner ',
-        textDirection: TextDirection.ltr,//ltr means left to right
-        style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.blue),
-      ),//Textbos must need text direction
+  //basic material app
+  runApp(MaterialApp(
+    theme: ThemeData(brightness: Brightness.light, primaryColor: Colors.blueGrey),
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Arpan created this APP",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.yellow[400],
+        centerTitle: true,
+        elevation: 20,
+      ),
+      body: const Center(child: Text("Welcome to this App",style: TextStyle(color: Colors.black87 , fontWeight: FontWeight.bold, fontSize: 25),
+      ),
+      )
+
+    ),
     ),
   );
 }
