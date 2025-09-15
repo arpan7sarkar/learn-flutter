590deb8654236b3d65c0e6e4723e10216e2fbb27
arpan7sarkar <arpansakar@gmail.com>
2025-09-15 19:33:26 +0530

    onTap change bg colour

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -3,61 +3,41 @@ import 'package:flutter/material.dart';
 const learner = 'Arpan';
 
 void main() {
-  //basic material app
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Trying on click")),
-        body: Center(child: UserButton(),),
-      ),
-    ),
-  );
+  runApp(MaterialApp(home: Myapp()));
 }
-//implemented StatelessWidget
-// class MyApp extends StatelessWidget{
-//   const MyApp({super.key});
 
-//   @override
-//    Widget build(BuildContext context){
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text(
-//           "Arpan created this APP",
-//           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-//         ),
-//         backgroundColor: Colors.yellow[400],
-//         centerTitle: true,
-//         elevation: 20,
-//       ),
-//       body: const Center(child: Text("Welcome to this App",style: TextStyle(color: Colors.black87 , fontWeight: FontWeight.bold, fontSize: 25),
-//       ),
-//       ),
-//     );
-//   }
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
 
-// }
+  @override
+  State<Myapp> createState() => _MyappState();
+}
 
-class UserButton extends StatelessWidget {
-  const UserButton({super.key});
+class _MyappState extends State<Myapp> {
+  bool isButtonPressed = false;
+
+  void _onTap() {
+    setState(() {
+      isButtonPressed  = !isButtonPressed ;
+    });
+  }
+
+  Color getBackgroundColor() {
+    return isButtonPressed
+        ? const Color.fromARGB(255, 0, 0, 0)
+        : Colors.pinkAccent;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        print("THe button is being tappped");
-      },
-      child: Container(
-        height: 40,
-        padding: EdgeInsets.all(8.0),
-        margin: EdgeInsets.symmetric(horizontal: 8.0),
-
-        decoration: BoxDecoration(
-          borderRadius:BorderRadius.circular(5),
-          color: Colors.greenAccent,
-
+    return Scaffold(
+      body: GestureDetector(
+        onTap: _onTap,
+        child: Container(
+          color: getBackgroundColor(),
         ),
-        child: Text("Click me"),
-
       ),
     );
   }
 }
+
